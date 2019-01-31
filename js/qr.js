c0dbc15ffaf0ca136dd9d5386a457606217c9023
“biggerbig <cyh21050@naver.com>
2019-01-31 23:26:59 +0900

    fasdf

--- js/qr.js
+++ js/qr.js
@@ -9,7 +9,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * //www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -1459,7 +1459,7 @@ var qrcode = function () {
                     null != g.callback && g.callback(g.result)
 
                     var qrTextData = $('#code_result').val();
-                    var replaceText = qrTextData.replace("//qr.nlotto.co.kr/?v=", "").replace("//qr.645lotto.net/?v=", "").replace(/a|b|c|d|e|f|g|h|i|j|k|l|n|o|p|q|r|s|t|u|v|w|x|y|z/gi, "m"); 
+                    var replaceText = qrTextData.replace("http://qr.nlotto.co.kr/?v=", "").replace("http://qr.645lotto.net/?v=", "").replace(/a|b|c|d|e|f|g|h|i|j|k|l|n|o|p|q|r|s|t|u|v|w|x|y|z/gi, "m"); 
                     var qs = replaceText
                     var round = 0;
                     var lotto_number = [];
